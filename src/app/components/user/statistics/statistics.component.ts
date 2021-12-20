1de7ab8f3a8bc31205f02443b54da249af3c2888
ignacio <ignasi44@hotmail.com>
2021-12-20 21:13:54 +0100

    Mejoradas estadisticas

--- src/app/components/user/statistics/statistics.component.ts
+++ src/app/components/user/statistics/statistics.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-statistics',
@@ -7,66 +6,126 @@ import { EChartsOption } from 'echarts';
   styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-  chartOption: EChartsOption = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-      },
-    ],
-  };
-
-  options2: any;
+  accesosGimnasioDatos: any;
+  genteSemanaDatos: any;
+  tiempoEntreneSemanaDatos: any;
+  rutinasCompletadasDatos: any;
   constructor() {}
 
   ngOnInit(): void {
-    const xAxisData = [];
-    const data1 = [];
-    const data2 = [];
-
-    for (let i = 0; i < 100; i++) {
-      xAxisData.push('category' + i);
-      data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-      data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    }
-
-    this.options2 = {
-      legend: {
-        data: ['bar', 'bar2'],
-        align: 'left',
-      },
-      tooltip: {},
-      xAxis: {
-        data: xAxisData,
-        silent: false,
-        splitLine: {
-          show: false,
+    this.accesosGimnasioDatos = {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
         },
       },
-      yAxis: {},
-      series: [
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      xAxis: [
         {
-          name: 'bar',
-          type: 'bar',
-          data: data1,
-          animationDelay: (idx: number) => idx * 1000,
-        },
-        {
-          name: 'bar2',
-          type: 'bar',
-          data: data2,
-          animationDelay: (idx: number) => idx * 100 + 100,
+          type: 'category',
+          data: [
+            'Ene',
+            'Feb',
+            'Mar',
+            'Abr',
+            'May',
+            'Jun',
+            'Jul',
+            'Ago',
+            'Set',
+            'Oct',
+            'Nov',
+            'Dic',
+          ],
+          axisTick: {
+            alignWithLabel: true,
+          },
         },
       ],
-      animationEasing: 'elasticOut',
-      animationDelayUpdate: (idx: number) => idx * 500,
+      yAxis: [
+        {
+          type: 'value',
+        },
+      ],
+      series: [
+        {
+          name: 'Accesos',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 12, 20, 13, 10, 30, 22, 17, 25, 6, 14, 3],
+        },
+      ],
+    };
+
+    this.genteSemanaDatos = {
+      xAxis: {
+        type: 'category',
+        data: ['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [45, 56, 50, 64, 35, 59, 78],
+          type: 'line',
+        },
+      ],
+    };
+
+    this.tiempoEntreneSemanaDatos = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Minutos',
+          data: [45, 56, 50, 64, 35, 59, 78],
+          type: 'bar',
+        },
+      ],
+    };
+
+    this.rutinasCompletadasDatos = {
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)',
+      },
+      legend: {
+        x: 'center',
+        y: 'bottom',
+        data: ['completadas', 'sin completar', 'pendientes'],
+      },
+      series: [
+        {
+          name: 'area',
+          type: 'pie',
+          radius: [30, 110],
+          roseType: 'area',
+          data: [
+            { value: 10, name: 'completadas' },
+            { value: 5, name: 'sin completar' },
+            { value: 15, name: 'pendientes' },
+          ],
+        },
+      ],
     };
   }
 }
